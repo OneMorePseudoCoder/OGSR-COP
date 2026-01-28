c3343dcf47ed5968b1387bca29c821fcfb66c0af
OneMorePseudoCoder <73041211+OneMorePseudoCoder@users.noreply.github.com>
2026-01-29 02:11:34 +0300

    Более надёжный CPatrolPathStorage
    Отсюда: https://github.com/solbjorn/reaper-engine/commit/7578b9692118a15cdd84804fc5e03e1125a0fb7a

--- ogsr_engine/COMMON_AI/PATH/path_manager_game_vertex.h
+++ ogsr_engine/COMMON_AI/PATH/path_manager_game_vertex.h
@@ -12,8 +12,7 @@
 #include "path_manager_params_game_vertex.h"
 
 template <typename _DataStorage, typename _dist_type, typename _index_type, typename _iteration_type>
-class CPathManager<CGameGraph, _DataStorage, SGameVertex<_dist_type, _index_type, _iteration_type>, _dist_type, _index_type, _iteration_type>
-    : public CPathManager<CGameGraph, _DataStorage, SBaseParameters<_dist_type, _index_type, _iteration_type>, _dist_type, _index_type, _iteration_type>
+class CPathManager<CGameGraph, _DataStorage, SGameVertex<_dist_type, _index_type, _iteration_type>, _dist_type, _index_type, _iteration_type> : public CPathManager<CGameGraph, _DataStorage, SBaseParameters<_dist_type, _index_type, _iteration_type>, _dist_type, _index_type, _iteration_type>
 {
 protected:
     typedef CGameGraph _Graph;
@@ -24,11 +23,10 @@ protected:
 
 protected:
     _Parameters* m_evaluator;
-    bool m_start_is_accessible;
+    bool m_start_is_accessible{true};
 
 public:
-    IC void setup(const _Graph* graph, _DataStorage* _data_storage, xr_vector<_index_type>* _path, const _index_type& _start_node_index, const _index_type& _goal_node_index,
-                  _Parameters& params);
+    IC void setup(const _Graph* graph, _DataStorage* _data_storage, xr_vector<_index_type>* _path, const _index_type& _start_node_index, const _index_type& _goal_node_index, _Parameters& params);
     IC bool is_accessible(const _index_type& vertex_id) const;
 };
 
--- ogsr_engine/COMMON_AI/PATH/path_manager_game_vertex_inline.h
+++ ogsr_engine/COMMON_AI/PATH/path_manager_game_vertex_inline.h
@@ -13,8 +13,7 @@
 #define CGameVertexPathManager CPathManager<CGameGraph, _DataStorage, SGameVertex<_dist_type, _index_type, _iteration_type>, _dist_type, _index_type, _iteration_type>
 
 TEMPLATE_SPECIALIZATION
-IC void CGameVertexPathManager::setup(const _Graph* _graph, _DataStorage* _data_storage, xr_vector<_index_type>* _path, const _index_type& _start_node_index,
-                                      const _index_type& _goal_node_index, _Parameters& parameters)
+IC void CGameVertexPathManager::setup(const _Graph* _graph, _DataStorage* _data_storage, xr_vector<_index_type>* _path, const _index_type& _start_node_index, const _index_type& _goal_node_index, _Parameters& parameters)
 {
     inherited::setup(_graph, _data_storage, _path, _start_node_index, _goal_node_index, parameters);
     m_evaluator = &parameters;
@@ -37,13 +36,12 @@ IC bool CGameVertexPathManager::is_accessible(const _index_type& vertex_id) cons
         Msg("! warning : empty vertex types");
     }
 #endif
-    auto I = m_evaluator->m_vertex_types->cbegin();
-    auto E = m_evaluator->m_vertex_types->cend();
-    for (; I != E; ++I)
-        if (graph->mask((*I).tMask, graph->vertex(vertex_id)->vertex_type()))
-            return (true);
 
-    return (false);
+    for (const auto& it : *(m_evaluator->m_vertex_types))
+        if (this->graph->mask(it.tMask, this->graph->vertex(vertex_id)->vertex_type()))
+            return true;
+
+    return false;
 }
 
 #undef TEMPLATE_SPECIALIZATION
